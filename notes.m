9aeb27ea57bb1a55c5a9fcfabc33a0856c40e918
Keiland <kc42@149-160-227-188.dhcp-bl.indiana.edu>
2017-03-05 01:23:49 -0500

    ud

---
+++ notes.m
@@ -0,0 +1,18 @@
+%Notes
+
+
+%downsample??
+%spikes?
+%1000 
+
+%30 layers
+%
+%decimate(time series, factor of(10))
+
+
+%Vector Autoregression
+%Granger Causality
+
+%average, decimating, 
+%periodogram
+%
