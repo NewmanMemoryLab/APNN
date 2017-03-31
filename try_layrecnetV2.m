9d2c4a379295f1fd5f2d174fc05281a68e6371d1
NewmanMemoryLab <memlab@indiana.edu>
2017-03-31 17:32:28 -0400

    ud

--- try_layrecnetV2.m
+++ try_layrecnetV2.m
@@ -1,29 +1,36 @@
 % Beginning script to run RNN (woo!)
 
 % Decimate and normalize ts
-ca1_ts = decimate(ca1_ts_m, 10);
-ca3_ts = decimate(ca3_ts_m, 10);
+% allCa1 = decimate(ca1_ts_m, 10);
+% allCa3 = decimate(ca3_ts_m, 10);
 
-ca1_n = ca1_ts/range(ca1_ts);
-ca3_n = ca3_ts/range(ca3_ts);
+% ca1_n = allCa1/range(allCa1);
+% ca3_n = allCa3/range(allCa3);
 
 % convert time series to cells (for some reason)
-ca3_c = con2seq(ca3_n');
-
-ca1_c = con2seq(ca1_n');
+ca3_c = con2seq(allCa3(:,1)');
+ca1_c = con2seq(allCa1(:,1)');
 
 %mb_size = 10000;
 train_start = 1;
 train_end = 100000;
-lrn_net = layrecnet(1, 50);
+eegLen = 10000;
+netLayers = 20;
+
+lrn_net = layrecnet(1, netLayers);
 lrn_net.trainFcn = 'trainbr';
 lrn_net.trainParam.epochs = 50;
 %lrn_net.performParam.regularization = 0.1;
+% lrn_net = train(lrn_net,ca3_c(train_start:train_end), ...
+%     ca1_c(train_start:train_end));
 
-lrn_net = train(lrn_net,ca3_c(train_start:train_end), ...
-    ca1_c(train_start:train_end));
+for i = 1:50
+    lrn_net = train(lrn_net, con2seq(allCa3(1:eegLen,i)'), con2seq(allCa1(1:eegLen,i)'));
+    fprintf('.')
+end
 
-start_t = 100000;
+
+start_t = 1;
 end_t = 160000;
 % Test output of recurrent neural net 
 test_out = lrn_net(ca3_c(start_t:end_t));
@@ -31,10 +38,12 @@ test_out = cell2mat(test_out);
 
 % See how ouput compares to real data
 % Get correlation
-emp_ts = ca1_n(start_t:end_t);
-[res, p_vals] = corrcoef([test_out' ca1_n(start_t:end_t)]);
+emp_ts = ca1_c(start_t:end_t);
+%[res, p_vals] = corrcoef([test_out' ca1_n(start_t:end_t)]);
+[res, p_vals] = corrcoef([test_out' cell2mat(ca1_c(start_t:end_t)')]);
 
 % Also test for peaks in data and find overlap
+
 emp_90 = prctile(emp_ts, 90);
 [~, emp_loc, emp_w] =  findpeaks(emp_ts, 'MinPeakHeight', emp_90);
 
